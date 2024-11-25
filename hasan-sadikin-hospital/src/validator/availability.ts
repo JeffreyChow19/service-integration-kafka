45e54ecc22493c492566cc33c4cecf434f4ab462
rachel <chelgchen@gmail.com>
2024-11-26 04:46:43 +0700

    chore: updated hasan sadikin to use postgres repo

--- hasan-sadikin-hospital/src/validator/availability.ts
+++ hasan-sadikin-hospital/src/validator/availability.ts
@@ -1,19 +1,29 @@
-import Joi, { ArraySchema } from '@hapi/joi';
+import Joi from '@hapi/joi';
 
-export const availabilityValidationSchema: ArraySchema = Joi.array()
-  .items(
-    Joi.object({
-      doctor: Joi.object({
-        name: Joi.string()
-          .required()
-          .error(new Error('Doctor name is required')),
-        specialization: Joi.string()
-          .required()
-          .error(new Error('Doctor specialization is required')),
-      }).required(),
-      time: Joi.string()
-        .required()
-        .error(new Error('Time is required')),
-    }),
-  )
-  .required();
+// Single Availability Schema
+export const availabilityValidationSchema = Joi.object({
+  doctor_name: Joi.string()
+    .required()
+    .error(new Error('Doctor name is required')),
+  specialization: Joi.string()
+    .required()
+    .error(new Error('Doctor specialization is required')),
+  time: Joi.string()
+    .required()
+    .error(new Error('Time is required')),
+});
+
+// Multiple Availability Schema
+export const availabilityValidationSchemaMany = Joi.array().items(
+  Joi.object({
+    doctor_name: Joi.string()
+      .required()
+      .error(new Error('Doctor name is required')),
+    specialization: Joi.string()
+      .required()
+      .error(new Error('Doctor specialization is required')),
+    time: Joi.string()
+      .required()
+      .error(new Error('Time is required')),
+  }),
+);
